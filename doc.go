2c9563da259091ead121d450f6e6b86f0cd5a36e
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2017-01-05 17:15:56 +0300

    Added const Version to doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,5 @@
 // Package reform is a better ORM for Go, based on non-empty interfaces and code generation.
 package reform
+
+// Version defines reform version.
+const Version = "v1.3.0-dev-active"
